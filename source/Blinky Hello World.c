5cb189a6098115d298aa589f720ea18317c43ba8
J. Scotty <jscottysolomon@gmail.com>
2023-09-03 15:13:21 -0700

    Adding in support for both LEDs and starting work on switches

--- source/Blinky Hello World.c
+++ source/Blinky Hello World.c
@@ -1,37 +1,17 @@
 /*
- * Copyright 2016-2023 NXP
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * o Redistributions of source code must retain the above copyright notice, this list
- *   of conditions and the following disclaimer.
- *
- * o Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
- *
- * o Neither the name of NXP Semiconductor, Inc. nor the names of its
- *   contributors may be used to endorse or promote products derived from this
- *   software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-/**
+ * @author:	J. Scotty Solomon
+ * @date:	09/23/23
  * @file    Blinky Hello World.c
  * @brief   Application entry point.
+ *
+ * Traffic light simulator. LEDs blink in order of red, green, and yellow.
+ * If a switch is pressed (or held down) only the yellow LED blinks whereas
+ * the other LEDs stay off.
+ *
+ * The red and green LEDs are the LEDs that are built into the FRDM board.
+ * The yellow LED is connected via a breadboard.
  */
+
 #include <stdio.h>
 #include "board.h"
 #include "peripherals.h"
@@ -39,9 +19,6 @@
 #include "clock_config.h"
 #include "MKL46Z4.h"
 #include "fsl_debug_console.h"
-/* TODO: insert other include files here. */
-
-/* TODO: insert other definitions and declarations here. */
 
 /*
  * @brief   Application entry point.
@@ -57,28 +34,53 @@ int main(void) {
     BOARD_InitDebugConsole();
 #endif
 
-    PRINTF("Hello World\n");
+    //LED1: Port D, Pin 5
+    //LED2: Port E, Pin 29
+    //SW1: Port C, Pin 3
+    //SW2: Port C, Pin 12
 
-    //Enbabling Port D for LED1
+    //Enabling Port C for SW1, SW2
+	SIM->SCGC5 |= 1<<11;
+
+    //Enabling Port D for LED1
     SIM->SCGC5 |= 1<<12;
 
     //Enabling Port E for LED2
     SIM->SCGC5 |= 1<<13;
 
-    //setup port D for GPIO
+    //set port C, pin 3 for GPIO
+//    PORTC->PCR[3] &= ~0x700;
+//	PORTC->PCR[3] |= 0x700 & (1 << 8);
+    PORTC->PCR[3] &= ~0x703;
+   	PORTC->PCR[3] |= 0x703 & ((1 << 8) | 0x3); //enable pull ups
+
+	//set port C, pin 12 for GPIO
+	PORTC->PCR[12] &= ~0x700;
+	PORTC->PCR[12] |= 0x700 & (1 << 8);
+
+    //set port D, pin 5 for GPIO
     PORTD->PCR[5] &= ~0x700;
     PORTD->PCR[5] |= 0x700 & (1 << 8);
 
-    //setup port E pin 29 as GPIO
+
+    //set port E, pin 29 for GPIO
     PORTE->PCR[29] &= ~0x700;
     PORTE->PCR[29] |= 0x700 & (1 << 8);
 
-    //set pin 5 Port C as output
+
+    //set port D, pin 5 as output
     GPIOD->PDDR |= (1<<5);
 
-    //set pin 29 Port E as output
+    //set port E, pin 29 as output
     GPIOE->PDDR |= (1<<29);
-    
+
+    //set port C, pin 3 as input
+    GPIOC->PDDR &= ~(1<<3);
+
+    //set port C, pin 12 as input
+    GPIOC->PDDR &= ~(1<<12);
+
+
 
     //LED2: PTE29
     //SIM_SCGC5 -> bit 13
@@ -88,8 +90,10 @@ int main(void) {
     /* Enter an infinite loop, just incrementing a counter. */
     while(1) {
         i++ ;
-
-        if (i % 100000 == 0) {
+        if(!(GPIOC->PDIR & 0x8)) {
+			GPIOD->PTOR |= (1<<5);
+        }
+        else if (i % 100000 == 0) {
         	GPIOD->PTOR |= (1<<5);
             GPIOE->PTOR |= (1<<29);
         }
