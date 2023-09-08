26247e30313ea2f159a59e7ab8e51f8773421860
J. Scotty <jscottysolomon@gmail.com>
2023-09-07 19:04:01 -0700

    Unsure

--- source/Blinky Hello World.c
+++ source/Blinky Hello World.c
@@ -21,36 +21,51 @@
 #include "fsl_debug_console.h"
 
 /*
- * @brief   Application entry point.
+ * The code below is taken from the video entitle "02 - Generating Events" and is
+ * used as a busy delay. I did not write the code for this function.
  */
+int delay_ms(unsigned short delay_t) {
+    SIM->SCGC6 |= (1 << 24); // Clock Enable TPM0
+    SIM->SOPT2 |= (0x2 << 24); // Set TPMSRC to OSCERCLK
+    TPM0->CONF |= (0x1 << 17); // Stop on Overflow
+    TPM0->SC = (0x1 << 7) | (0x07); // Reset Timer Overflow Flag, Set Prescaler 128
+    TPM0->MOD = delay_t * 61 + delay_t/2; //
+
+    TPM0->SC |= 0x01 << 3; // Start the clock!
+
+    while(!(TPM0->SC & 0x80)){} // Wait until Overflow Flag
+    return 1;
+}
+
 int main(void) {
 
     /* Init board hardware. */
     BOARD_InitBootPins();
     BOARD_InitBootClocks();
     BOARD_InitBootPeripherals();
-#ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL
+
     /* Init FSL debug console. */
-    BOARD_InitDebugConsole();
-#endif
+	#ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL
+		BOARD_InitDebugConsole();
+	#endif
 
     //LED1: Port D, Pin 5
     //LED2: Port E, Pin 29
+    //LED3: Port A, Pin 12
     //SW1: Port C, Pin 3
     //SW2: Port C, Pin 12
 
-    //Enabling Port C for SW1, SW2
-	SIM->SCGC5 |= 1<<11;
+    //Enabling Ports
+    SIM->SCGC5 |= 1<<9; 	//Port A for LED3
+	SIM->SCGC5 |= 1<<11; 	//Port C for SW1, SW2
+    SIM->SCGC5 |= 1<<12;	//Port D for LED1
+    SIM->SCGC5 |= 1<<13; 	//Port E for LED2
 
-    //Enabling Port D for LED1
-    SIM->SCGC5 |= 1<<12;
-
-    //Enabling Port E for LED2
-    SIM->SCGC5 |= 1<<13;
+    //set port A, pin 3 for GPIO
+    PORTA->PCR[12] &= ~0x700;
+	PORTA->PCR[12] |= 0x700 & (1 << 8);
 
     //set port C, pin 3 for GPIO
-//    PORTC->PCR[3] &= ~0x700;
-//	PORTC->PCR[3] |= 0x700 & (1 << 8);
     PORTC->PCR[3] &= ~0x703;
    	PORTC->PCR[3] |= 0x703 & ((1 << 8) | 0x3); //enable pull ups
 
@@ -62,40 +77,61 @@ int main(void) {
     PORTD->PCR[5] &= ~0x700;
     PORTD->PCR[5] |= 0x700 & (1 << 8);
 
-
     //set port E, pin 29 for GPIO
     PORTE->PCR[29] &= ~0x700;
     PORTE->PCR[29] |= 0x700 & (1 << 8);
 
+    //Setting output
+    GPIOA->PDDR |= (1<<12);	//port A, pin 1
+    GPIOD->PDDR |= (1<<5);	//port D, pin 5
+    GPIOE->PDDR |= (1<<29);	//port E, pin 29
 
-    //set port D, pin 5 as output
-    GPIOD->PDDR |= (1<<5);
-
-    //set port E, pin 29 as output
-    GPIOE->PDDR |= (1<<29);
-
-    //set port C, pin 3 as input
-    GPIOC->PDDR &= ~(1<<3);
-
-    //set port C, pin 12 as input
-    GPIOC->PDDR &= ~(1<<12);
-
-
-
-    //LED2: PTE29
-    //SIM_SCGC5 -> bit 13
+    //Setting input
+    GPIOC->PDDR &= ~(1<<3);	//port C, pin 3
+    GPIOC->PDDR &= ~(1<<12);//port C, pin 12
 
     /* Force the counter to be placed into memory. */
     volatile static int i = 0 ;
+    volatile static int x = 0 ;
+    volatile static int test = 0 ;
     /* Enter an infinite loop, just incrementing a counter. */
+
+//    GPIOD->PTOR &= ~(1<<5);
+//	GPIOE->PTOR &= ~(1<<29);
+
     while(1) {
-        i++ ;
-        if(!(GPIOC->PDIR & 0x8)) {
-			GPIOD->PTOR |= (1<<5);
+        i++;
+
+        if(!(GPIOC->PDIR & 0x8) || x == 1) {
+        	GPIOD->PDOR |= (1<<5);
+        	GPIOE->PDOR |= (1<<29);
+        	x = 1;
         }
-        else if (i % 100000 == 0) {
-        	GPIOD->PTOR |= (1<<5);
-            GPIOE->PTOR |= (1<<29);
+//        else {
+//        	x = 0;
+//        }
+
+		if (x == 1) {
+			GPIOA->PDOR |= (1<<12);
+			test += delay_ms(1000);
+//			GPIOA->PDOR &= ~(1<<12);
+		}
+//		else if (i % 100000 == 0) {
+		else {
+
+        	GPIOD->PDOR |= (1<<5);
+        	test += delay_ms(1000);
+        	GPIOD->PDOR &= ~(1<<5);
+//        	test += delay_ms(10);
+
+        	GPIOA->PDOR |= (1<<12);
+			test += delay_ms(1000);
+			GPIOA->PDOR &= ~(1<<12);
+//			delay_ms(1000);
+
+            GPIOE->PDOR |= (1<<29);
+            test += delay_ms(1000);
+            GPIOE->PDOR &= ~(1<<29);
         }
     }
     return 0 ;
