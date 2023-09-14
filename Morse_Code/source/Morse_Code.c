4c3658d5917b42a606b424dd897bce64088cd885
J. Scotty <jscottysolomon@gmail.com>
2023-09-14 14:22:06 -0700

    Adding in switch reset support and adding in documentation

--- Morse_Code/source/Morse_Code.c
+++ Morse_Code/source/Morse_Code.c
@@ -1,36 +1,8 @@
-/*
- * Copyright 2016-2023 NXP
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * o Redistributions of source code must retain the above copyright notice, this list
- *   of conditions and the following disclaimer.
- *
- * o Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
- *
- * o Neither the name of NXP Semiconductor, Inc. nor the names of its
- *   contributors may be used to endorse or promote products derived from this
- *   software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 /**
- * @file    Morse_Code.c
- * @brief   Application entry point.
+ * @author J. Scotty Solomon
+ * @date 09-September-23
+ *
+ *
  */
 #include <stdio.h>
 #include <string.h>
@@ -42,6 +14,10 @@
 #include "MKL46Z4.h"
 #include "fsl_debug_console.h"
 
+#define PERIOD_DELAY 600
+#define LETTER_LENGTH 3
+#define NUMBER_LENGTH 4
+
 const char* letterMorseCode[] = {
     ".-XX",  // A
     "-...",  // B
@@ -49,23 +25,23 @@ const char* letterMorseCode[] = {
     "-..",   // D
     ".XXX",  // E
     "..-.",  // F
-    "--.X",   // G
+    "--.X",  // G
     "....",  // H
-    "..XX",    // I
+    "..XX",  // I
     ".---",  // J
-    "-.-X",   // K
+    "-.-X",  // K
     ".-..",  // L
-    "--XX",    // M
-    "-.XX",    // N
-    "---X",   // O
+    "--XX",  // M
+    "-.XX",  // N
+    "---X",  // O
     ".--.",  // P
     "--.-",  // Q
-    ".-.X",   // R
-    "...X",   // S
-    "-XXX",     // T
-    "..-X",   // U
-    "...-X",  // V
-    ".--X",   // W
+    ".-.X",  // R
+    "...X",  // S
+    "-XXX",  // T
+    "..-X",  // U
+    "...-X", // V
+    ".--X",  // W
     "-..-",  // X
     "-.--",  // Y
     "--.."   // Z
@@ -89,8 +65,72 @@ int red = 0; int green = 0;
 void switchLED();
 void delay_ms(unsigned short);
 void displayMorse(char *message, int length);
-void handleMorse(char* morse, int len);
+void handleMorse(const char* morse, int len);
 
+/**
+ * Enables input/output for LED1/2 and SW1. Enters a
+ * permanent loop in which a string is displayed as
+ * morse code once. String can be displayed again by
+ * pressing SW1.
+ */
+int main(void) {
+
+    /* Init board hardware. */
+    BOARD_InitBootPins();
+    BOARD_InitBootClocks();
+    BOARD_InitBootPeripherals();
+
+    /* Init FSL debug console. */
+	#ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL
+		BOARD_InitDebugConsole();
+	#endif
+
+	//Enabling Ports
+    SIM->SCGC5 |= 1<<12;	//Port D for LED1
+	SIM->SCGC5 |= 1<<13; 	//Port E for LED2
+	SIM->SCGC5 |= 1<<11; 	//Port C for SW1
+
+	//set port D, pin 5 for GPIO
+	PORTD->PCR[5] &= ~0x700;
+	PORTD->PCR[5] |= 0x700 & (1 << 8);
+
+	//set port E, pin 29 for GPIO
+	PORTE->PCR[29] &= ~0x700;
+	PORTE->PCR[29] |= 0x700 & (1 << 8);
+
+	//set port C, pin 3 for GPIO
+	PORTC->PCR[3] &= ~0x703;
+	PORTC->PCR[3] |= 0x703 & ((1 << 8) | 0x3); //enable pull ups
+
+	//Setting input
+	GPIOC->PDDR &= ~(1<<3);	//port C, pin 3
+
+	int i = 0;
+
+    while(1) {
+    	char * str = "CSE 325";
+
+    	if(i == 0) {
+    		red = green = 0;
+    		displayMorse(str,strlen(str));
+    	}
+
+    	if(!(GPIOC->PDIR & 0x8)) {
+    		red = green = 0; //resetting LEDs
+    		displayMorse(str,strlen(str));
+    	}
+    	i++;
+    }
+
+    return 0 ;
+}
+
+/**
+ * Delays clock by desired miliseconds. This code is from
+ * the lecture videos. I did not write it.
+ *
+ * @delay_t delay amount in miliseconds
+ */
 void delay_ms(unsigned short delay_t) {
     SIM->SCGC6 |= (1 << 24); // Clock Enable TPM0
     SIM->SOPT2 |= (0x2 << 24); // Set TPMSRC to OSCERCLK
@@ -104,29 +144,32 @@ void delay_ms(unsigned short delay_t) {
     return;
 }
 
+/**
+ * Takes a string and outputs its morse code representation
+ * on LED1.
+ *
+ * @message string to be converted to morse code
+ * @length length of desired string
+ */
 void displayMorse(char *message, int length) {
-	char i = 0;
+	int i = 0;
 
 	while(message[i] != '.') {
-		char curr = message[i];
+		unsigned char curr = message[i];
 
 		if(curr == ' ') {
-			//delay
-//			delay_ms(600);
 			switchLED();
 		}
 
 		if(curr >= '0' && curr <= '9') {
-//			printf("%s ", intMorseCode[curr - '0']);
-			handleMorse(intMorseCode[curr - '0'], 4);
+			handleMorse(intMorseCode[curr - '0'], NUMBER_LENGTH);
 		}
 
 		curr = toupper(message[i]);
 
 		if(curr >= 'A' && curr <= 'Z') {
 			int index = curr - 'A';
-//			printf("%s ",letterMorseCode[index]);
-			handleMorse(letterMorseCode[index],2);
+			handleMorse(letterMorseCode[index],LETTER_LENGTH);
 
 		}
 
@@ -136,8 +179,22 @@ void displayMorse(char *message, int length) {
 			break;
 		}
 	}
+
+	switchLED();
+	if(red) {
+		GPIOE->PDDR |= (1<<29);
+		delay_ms(PERIOD_DELAY);
+		GPIOE->PDDR &= ~(1<<29);
+	} else {
+		GPIOD->PDDR |= (1<<5);
+		delay_ms(PERIOD_DELAY);
+		GPIOD->PDDR &= ~(1<<5);
+	}
 }
 
+/**
+ * Switches which LED should be turned on next.
+ */
 void switchLED() {
 	if(!red && ! green) {
 		red = 1;
@@ -150,7 +207,13 @@ void switchLED() {
 	}
 }
 
-void handleMorse(char* morse, int len) {
+/**
+ * Outputs the morse code for a single character
+ *
+ * @morse morse code representation of a character in a string
+ * @len max length of morse code string
+ */
+void handleMorse(const char* morse, int len) {
 	for(int i = 0; i <= len; i++) {
 		int delay = 0;
 
@@ -175,52 +238,4 @@ void handleMorse(char* morse, int len) {
 	}
 
 	delay_ms(600);
-}
-
-/*
- * @brief   Application entry point.
- */
-int main(void) {
-
-    /* Init board hardware. */
-    BOARD_InitBootPins();
-    BOARD_InitBootClocks();
-    BOARD_InitBootPeripherals();
-
-    /* Init FSL debug console. */
-	#ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL
-		BOARD_InitDebugConsole();
-	#endif
-
-    SIM->SCGC5 |= 1<<12;	//Port D for LED1
-	SIM->SCGC5 |= 1<<13; 	//Port E for LED2
-
-	//set port D, pin 5 for GPIO
-	PORTD->PCR[5] &= ~0x700;
-	PORTD->PCR[5] |= 0x700 & (1 << 8);
-
-	//set port E, pin 29 for GPIO
-	PORTE->PCR[29] &= ~0x700;
-	PORTE->PCR[29] |= 0x700 & (1 << 8);
-
-//	GPIOD->PDDR |= (1<<5);	//port D, pin 5
-//	GPIOE->PDDR |= (1<<29);	//port E, pin 29
-
-//    i
-
-    while(1) {
-//    	delay_ms(1000);
-//        GPIOE->PDDR &= ~(1<<29);
-//        delay_ms(1000);
-//        GPIOE->PDDR |= (1<<29);
-
-    	char* test = "SOS SOS SOS SOS.";
-
-    	displayMorse(test,26);
-
-    	delay_ms(10000);
-    	switchLED();
-    }
-
-    return 0 ;
 }
