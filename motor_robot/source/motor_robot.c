4a14d920ce2a47930071a77ae24599e215e80b74
JScotty <jscottysolomon@gmail.com>
2023-10-02 15:08:16 -0700

    Adding initial staright s pattern, untested

--- motor_robot/source/motor_robot.c
+++ motor_robot/source/motor_robot.c
@@ -14,7 +14,17 @@
 #include "clock_config.h"
 #include "MKL46Z4.h"
 #include "fsl_debug_console.h"
-/* TODO: insert other include files here. */
+
+#define STRAIGHT_TIME 500
+#define STOP_TIME 100
+#define TURN_TIME 50
+
+void delay_ms(unsigned short delay_t);
+void straight();
+void stop();
+void figure1();
+void turn_left();
+void turn_right();
 
 /*
  * Pin Mappings:
@@ -79,6 +89,10 @@ int main(void) {
 	PORTC->PCR[2] &= ~0x700;
 	PORTC->PCR[2] |= 0x700 & (1 << 8);
 
+	//C3
+	PORTC->PCR[3] &= ~0x703;
+	PORTC->PCR[3] |= 0x703 & ((1 << 8) | 0x3); //enable pull ups
+
 	//Setting up pins for GPIO output
 	GPIOA->PDDR |= (1<<15);	//A15
 	GPIOA->PDDR |= (1<<12);	//A12
@@ -87,33 +101,140 @@ int main(void) {
 	GPIOB->PDDR |= (1<<2);	//B2
 	GPIOC->PDDR |= (1<<1);	//C1
 	GPIOC->PDDR |= (1<<2);	//C2
-	GPIOC->PDDR |= (1<<3);	//C3
+	// GPIOC->PDDR |= (1<<3);	//C3
+
+	//Setting input
+	GPIOC->PDDR &= ~(1<<3);	//C3
 
 
 	//Turning on things
-	GPIOA->PDOR |= (1<<15); //A15
-	GPIOA->PDOR |= (1<<12); //A12
+	// GPIOA->PDOR |= (1<<15); //A15
+	// GPIOA->PDOR |= (1<<12); //A12
 
-	GPIOB->PDOR |= (1<<1); //B1
-	GPIOB->PDOR |= (1<<2); //B2
+	// GPIOB->PDOR |= (1<<1); //B1
+	// GPIOB->PDOR |= (1<<2); //B2
 
-	GPIOC->PDOR |= (1<<1); //C1
-	GPIOC->PDOR |= (1<<2); //C2
+	// GPIOC->PDOR |= (1<<1); //C1
+	// GPIOC->PDOR |= (1<<2); //C2
 
 	#ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL
 		BOARD_InitDebugConsole();
 	#endif
 
-    PRINTF("Hello World\n");
-
-    /* Force the counter to be placed into memory. */
-    volatile static int i = 0 ;
-    /* Enter an infinite loop, just incrementing a counter. */
     while(1) {
-        i++ ;
-        /* 'Dummy' NOP to allow source level single stepping of
-            tight while() loop */
-        __asm volatile ("nop");
+		if(!(GPIOC->PDIR & 0x8)) {
+			figure1();
+		}
+
     }
     return 0 ;
+}
+
+/** Straight S*/
+void figure1() {
+	//Straight IN1, PMW, STBY
+	straight();
+	delay_ms(STRAIGHT_TIME);
+	stop();
+	delay_ms(STOP_TIME);
+
+	//Left Turn, A
+	turn_left();
+	delay_ms(TURN_TIME);
+	stop();
+	delay_ms(STOP_TIME);
+
+	//Straight
+	straight();
+	delay_ms(STRAIGHT_TIME);
+	stop();
+	delay_ms(STOP_TIME);
+
+	//Left Turn
+	turn_left();
+	delay_ms(TURN_TIME);
+	stop();
+	delay_ms(STOP_TIME);
+
+	//Straight
+	straight();
+	delay_ms(STRAIGHT_TIME);
+	stop();
+	delay_ms(STOP_TIME);
+
+	//Right Turn
+	turn_right();
+	delay_ms(TURN_TIME);
+	stop();
+	delay_ms(STOP_TIME);
+
+	//Straight
+	straight();
+	delay_ms(STRAIGHT_TIME);
+	stop();
+	delay_ms(STOP_TIME);
+
+	//Right Turn
+	turn_right();
+	delay_ms(TURN_TIME);
+	stop();
+	delay_ms(STOP_TIME);
+
+	//Straight
+	straight();
+	delay_ms(STRAIGHT_TIME);
+	stop();
+	delay_ms(STOP_TIME);
+
+	//Turn Off
+		//A
+		GPIOB->PDOR &= ~(1<<2); //B2
+		GPIOB->PDOR &= ~(1<<1); //B1
+
+		//B
+		GPIOC->PDOR &= ~(1<<1); //C1
+		GPIOB->PDOR &= ~(1<<3); //B3
+}
+
+void stop() {
+	//A
+	GPIOB->PDOR &= ~(1<<2); //B2
+	GPIOB->PDOR &= ~(1<<1); //B1
+
+	//B
+	GPIOC->PDOR &= ~(1<<1); //C1
+	GPIOB->PDOR &= ~(1<<3); //B3
+}
+
+void straight() {
+	//A
+	GPIOB->PDOR |= (1<<2); //B2
+	GPIOB->PDOR |= (1<<1); //B1
+
+	//B
+	GPIOC->PDOR |= (1<<1); //C1
+	GPIOB->PDOR |= (1<<3); //B3
+}
+
+void turn_left() {
+	GPIOB->PDOR |= (1<<2); //B2
+	GPIOB->PDOR |= (1<<1); //B1
+}
+
+void turn_right() {
+	GPIOB->PDOR |= (1<<3); //B3
+	GPIOC->PDOR |= (1<<1); //C1
+}
+
+void delay_ms(unsigned short delay_t) {
+    SIM->SCGC6 |= (1 << 24); // Clock Enable TPM0
+    SIM->SOPT2 |= (0x2 << 24); // Set TPMSRC to OSCERCLK
+    TPM0->CONF |= (0x1 << 17); // Stop on Overflow
+    TPM0->SC = (0x1 << 7) | (0x07); // Reset Timer Overflow Flag, Set Prescaler 128
+    TPM0->MOD = delay_t * 61 + delay_t/2; //
+
+    TPM0->SC |= 0x01 << 3; // Start the clock!
+
+    while(!(TPM0->SC & 0x80)){} // Wait until Overflow Flag
+    return;
 }
